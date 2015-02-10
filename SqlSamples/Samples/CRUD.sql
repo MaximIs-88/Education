6063a86f5d4d3c67dd8cf52706378828f6cd124d
maximIs-88 <isachenkomax@gmail.com>
2015-02-11 00:38:55 +0300

    ffg

---
+++ SqlSamples/Samples/CRUD.sql
@@ -0,0 +1,28 @@
+﻿
+CREATE DATABASE [t1] ON  PRIMARY 
+( NAME = N't2',
+  FILENAME = N'C:\USERS\МИША\SOURCE\REPOS\EDUCATION\SQLSAMPLES\SAMPLES\t3.mdf')
+
+  DROP DATABASE t1;
+
+SELECT *
+FROM sys.databases
+
+CREATE TABLE Persons
+(
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);
+
+INSERT INTO Role (Code, Name)
+VALUES ('admin', 'Администратор')
+
+DELETE FROM User1
+WHERE ID =1
+
+
+update TableTest set first = 'update' where id = 3
+select * from TableTest
---
+++ SqlSamples/Samples/HavingGroupBy.sql
@@ -0,0 +1,20 @@
+﻿ --HAVING
+ --HAVING — необязательный (опциональный) параметр оператора SELECT для указания условия
+ -- на результат агрегатных функций (MAX, SUM, AVG, …).
+ --Распространённая ошибка — указание в SELECT столбца, пропущенного в GROUP BY.
+ 
+ SELECT DeptID, SUM(SaleAmount) FROM Sales
+ WHERE SaleDate = '01-Jan-2000'
+ GROUP BY DeptID
+ HAVING SUM(SaleAmount) > 1000
+
+ SELECT DepartmentName, COUNT(*) 
+ FROM employee,department 
+ WHERE employee.DepartmentID = department.DepartmentID 
+ GROUP BY DepartmentName
+ HAVING COUNT(*)>1;
+
+ select TableTest.first, COUNT(TableTest.Id) as countValue
+ from TableTest
+ group by TableTest.first
+ having COUNT(TableTest.Id) > 0
