618ac679c9539e3607fbdf52be195e8c13864d48
maximIs-88 <isachenkomax@gmail.com>
2015-02-10 18:22:45 +0300

    ggh

---
+++ SqlSamples/Samples/Joins.sql
@@ -0,0 +1,23 @@
+﻿select *
+from TableTest
+where id > 1 and id < 3
+
+select *
+from TableTest as t1
+inner join Test1 as t2 on  t1.Id = t2.Id
+--where t1.id > 1 and t1.id < 3
+
+select *
+from TableTest as t1
+right outer join Test1 as t2 on  t1.Id = t2.Id
+--where t1.id > 1 and t1.id < 3
+
+select *
+from TableTest as t1
+full outer join Test1 as t2 on  t1.Id = t2.Id
+
+select *
+from TableTest as t1
+cross join Test1 as t2
+
+
