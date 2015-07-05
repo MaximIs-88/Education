2fc11320a2243a49619d839c93ecad50a2f67852
maximIs-88 <isachenkomax@gmail.com>
2015-07-05 18:11:54 +0200

    cccc

---
+++ AspNETMVCTest/AspNETMVCTest/Models/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AspNETMVCTest.Models
+{
+    internal interface IBookRepository
+    {
+        List<BookDetails> Books { get; }
+
+        void Add(BookDetails newBook);
+    }
+}
---
+++ SharpBase/SharpBase/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharpBase
+{
+    [TestClass]
+    public class Class1
+    {
+        private int a = 1;
+        
+        [TestMethod]
+        public void Test()
+        {
+            var b = a++;
+            Assert.AreEqual(1, b);
+            Assert.AreEqual(2, a);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var b = a + 1;
+            Assert.AreEqual(2, b);
+            Assert.AreEqual(1, a);
+
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var b = ++a;
+            Assert.AreEqual(2, b);
+            Assert.AreEqual(2, a);
+        }
+    }
+}
